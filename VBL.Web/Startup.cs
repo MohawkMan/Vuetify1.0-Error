9ff1f6ae69132e411d1fa5739dec1b88f063a1e8
MohawkMan <jon@volleyballlife.com>
2017-11-09 19:51:10 -0800

    Test

--- VBL.Web/Startup.cs
+++ VBL.Web/Startup.cs
@@ -20,7 +20,7 @@ namespace VBL.Web
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-        }
+        } 
 
         public IConfiguration Configuration { get; }
 
